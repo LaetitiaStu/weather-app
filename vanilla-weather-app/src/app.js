a9acdb9d87d7a0843f17bec3280f2f9faf0013e4
LaetitiaStu <129389279+LaetitiaStu@users.noreply.github.com>
2023-04-30 17:51:49 +0200

    added the search engine

--- vanilla-weather-app/src/app.js
+++ vanilla-weather-app/src/app.js
@@ -31,9 +31,20 @@ function displayTemperature(response) {
     iconElement.setAttribute("alt", response.data.weather[0].description)
 }
 
+function search(city) {
 let apiKey = "38f9fc1db6e0156a5e7d0c4ef4824562";
-
-let city = "New York"
 let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
-
 axios.get(apiUrl).then(displayTemperature);
+
+}
+
+function handleSubmit (event) {
+    event.preventDefault();
+    let cityInputElement = document.querySelector("#city-input");
+    search(cityInputElement.value)
+}
+
+search("New York")
+
+let form = document.querySelector ("#search-form");
+form.addEventListener("submit", handleSubmit);
