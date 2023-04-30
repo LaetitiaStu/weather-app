cfb3c382838c8ff145ac25ed25815830c15caada
LaetitiaStu <129389279+LaetitiaStu@users.noreply.github.com>
2023-04-30 17:33:49 +0200

    Added relevant icon from the API

--- vanilla-weather-app/src/app.js
+++ vanilla-weather-app/src/app.js
@@ -19,15 +19,21 @@ function displayTemperature(response) {
     let humidityElement = document.querySelector("#humidity")
     let speedElement = document.querySelector("#wind")
     let dateElement = document.querySelector("#date")
+    let iconElement = document.querySelector("#icon")
+
     temperatureElement.innerHTML = Math.round(response.data.main.temp);
     cityElement.innerHTML = response.data.name;
     descriptionElement.innerHTML = response.data.weather[0].description;
     humidityElement.innerHTML = response.data.main.humidity;
     speedElement.innerHTML = Math.round(response.data.wind.speed);
     dateElement.innerHTML = formatDate(response.data.dt *1000);
+    iconElement.setAttribute("src", `http://openweathermap.org/img/wn/${response.data.weather[0].icon}.png`);
+    iconElement.setAttribute("alt", response.data.weather[0].description)
 }
 
 let apiKey = "38f9fc1db6e0156a5e7d0c4ef4824562";
-let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=New York&appid=${apiKey}&units=metric`;
+
+let city = "New York"
+let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
 
 axios.get(apiUrl).then(displayTemperature);
