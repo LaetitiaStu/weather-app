174f5f15ba6cf5f565e053292f4456470679a76f
LaetitiaStu <129389279+LaetitiaStu@users.noreply.github.com>
2023-05-07 17:40:32 +0200

    Debugging

--- vanilla-weather-app/src/app.js
+++ vanilla-weather-app/src/app.js
@@ -21,22 +21,24 @@ let days = ["Sun", "Mun", "Tue", "Wed", "Thu", "Fri", "Sat"]
 return days[day];
 }
 
-function displayForecast (response) {
+function displayForecast(response) {
     let forecast = response.data.daily;
 
     let forecastElement = document.querySelector("#forecast");
 
+
+
     let days = ["Thu", "Fri", "Sat", "Sun"];
 
     let forecastHTML = `<div class = "row">`;
-    forecast.forEach(function(forecastDay, index) {
-        if (index < 6){
+    forecast.forEach(function (forecastDay, index) {
+        if (index < 6) {
         forecastHTML = forecastHTML + 
     `
               <div class="col-2">
-                <div class="weather-forecast-date">${formatDay(forecastDay).dt}</div>
+                <div class="weather-forecast-date">${formatDay(forecastDay.dt)}</div>
                 <img
-                  src="http://openweathermap.org/img/wn/${forecastDay.weather[0].icon}10d.png"
+                  src="http://openweathermap.org/img/wn/${forecastDay.weather[0].icon}@2x.png"
                   alt=""
                   width="42px"
                 />
@@ -56,7 +58,7 @@ function displayForecast (response) {
 
 function getForecast (coordinates) {
 console.log(coordinates);
-let apiKey = "38f9fc1db6e0156a5e7d0c4ef4824562";
+let apiKey = "a95c2c6739994ba4903e007ee817e7d1";
 let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${coordinates.lat}&lon=${coordinates.lon}&appid=${apiKey}&units=metric`;
 console.log(apiUrl)
 axios.get(apiUrl).then(displayForecast);
@@ -129,4 +131,3 @@ let celsiusLink = document.querySelector("#celsius-link");
 celsiusLink.addEventListener ("click", displayCelsiusTemperature);
 
 search("New York");
-displayForecast();
